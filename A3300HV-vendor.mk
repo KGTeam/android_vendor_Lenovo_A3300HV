dd1a0d17ad143dc2416a2b30779c1e88c806a08a
Ilya <ilya.lebedev.2003@mail.ru>
2016-04-13 13:07:02 +0600

    Initial Release

---
+++ A3300HV-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/A3300HV/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/A3300HV/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/A3300HV/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/A3300HV/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Lenovo/A3300HV/xbin,system/xbin)
+
